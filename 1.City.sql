53a6bd207a840b94777d91b31f3a7883892286bf
SakshiSupekar07 <supekarsakshj07@gmail.com>
2025-09-02 11:01:48 +0530

    Basic Select

--- 1.City.sql
+++ 1.City.sql
@@ -1,3 +1,3 @@
-//Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
+--Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
 
 Select * from CITY where Population>100000 and CountryCode='USA';
