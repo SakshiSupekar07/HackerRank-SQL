83fe1ce9f9fde265350bcf2ce226ac6ffe3b19b0
SakshiSupekar07 <supekarsakshj07@gmail.com>
2025-09-10 09:48:27 +0530

    Basic Select

---
+++ 8.Weather Observation.sql
@@ -0,0 +1,24 @@
+--Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT city
+FROM station
+WHERE city NOT LIKE 'A%'
+  AND city NOT LIKE 'E%'
+  AND city NOT LIKE 'I%'
+  AND city NOT LIKE 'O%'
+  AND city NOT LIKE 'U%'
+  AND city NOT LIKE 'a%'
+  AND city NOT LIKE 'e%'
+  AND city NOT LIKE 'i%'
+  AND city NOT LIKE 'o%'
+  AND city NOT LIKE 'u%'
+   -- OR condition for ending with vowel
+   OR city NOT LIKE '%A'
+  AND city NOT LIKE '%E'
+  AND city NOT LIKE '%I'
+  AND city NOT LIKE '%O'
+  AND city NOT LIKE '%U'
+  AND city NOT LIKE '%a'
+  AND city NOT LIKE '%e'
+  AND city NOT LIKE '%i'
+  AND city NOT LIKE '%o'
+  AND city NOT LIKE '%u';
